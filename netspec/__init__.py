c2e0e70fd9355696c008b4e7d1b1daffbe709f60
J. Michael Burgess <jmichaelburgess@gmail.com>
2022-12-30 09:12:54 +0100

    getting things going

--- netspec/__init__.py
+++ netspec/__init__.py
@@ -8,3 +8,6 @@ __email__ = 'jburgess@mpe.mpg.de'
 
 from .utils.configuration import netspec_config, show_configuration
 from .utils.logging import update_logging_level, activate_warnings, silence_warnings
+
+from . import _version
+__version__ = _version.get_versions()['version']
